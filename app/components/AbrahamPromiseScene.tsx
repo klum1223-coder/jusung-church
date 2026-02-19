e5e73a1d6e314ab08b9fab826c0b9f5d9e0709ea
klum3 <klum1223@gmail.com>
2026-02-19 15:48:57 +0900

    fix: remove large 3D cross from background scene

--- app/components/AbrahamPromiseScene.tsx
+++ app/components/AbrahamPromiseScene.tsx
@@ -5,46 +5,7 @@ import { Canvas, useFrame } from '@react-three/fiber';
 import { Stars, Float, Sparkles } from '@react-three/drei';
 import * as THREE from 'three';
 
-function GlowingCrossModel() {
-    const group = useRef<THREE.Group>(null);
 
-    useFrame((state) => {
-        if (group.current) {
-            const t = state.clock.getElapsedTime();
-            group.current.rotation.y = Math.sin(t * 0.1) * 0.1;
-            group.current.rotation.z = Math.sin(t * 0.05) * 0.05;
-        }
-    });
-
-    return (
-        <group ref={group}>
-            <Float speed={1.5} rotationIntensity={0.2} floatIntensity={0.5} floatingRange={[-0.2, 0.2]}>
-                {/* Vertical Beam */}
-                <mesh position={[0, 0, 0]}>
-                    <boxGeometry args={[0.3, 4, 0.3]} />
-                    <meshStandardMaterial
-                        color="#fbbf24"
-                        emissive="#b45309"
-                        emissiveIntensity={2}
-                        toneMapped={false}
-                    />
-                </mesh>
-                {/* Horizontal Beam */}
-                <mesh position={[0, 0.8, 0]}>
-                    <boxGeometry args={[2.5, 0.3, 0.3]} />
-                    <meshStandardMaterial
-                        color="#fbbf24"
-                        emissive="#b45309"
-                        emissiveIntensity={2}
-                        toneMapped={false}
-                    />
-                </mesh>
-                {/* Central Core Light */}
-                <pointLight position={[0, 0.8, 0.5]} color="#fbbf24" intensity={5} distance={10} decay={2} />
-            </Float>
-        </group>
-    );
-}
 
 export default function AbrahamPromiseScene() {
     return (
@@ -78,7 +39,7 @@ export default function AbrahamPromiseScene() {
                 />
 
                 <ambientLight intensity={0.1} />
-                <GlowingCrossModel />
+
 
                 <fog attach="fog" args={['#020205', 5, 25]} />
             </Canvas>
